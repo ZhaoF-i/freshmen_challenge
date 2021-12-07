48ffd1991f0bca98c7f1edae228f6e14fd492d93
Zhaofei <1281041324@qq.com>
2021-12-07 16:37:25 +0800

    成功修改了logging

--- utils/log.py
+++ utils/log.py
@@ -38,35 +38,3 @@ class log():
 
         logging.config.dictConfig(self.LOGGING)
 
-# LOGGING = {
-#     'version': 1,
-#     'formatters': {
-#         'default': {
-#             'format': '%(asctime)s [%(levelname)-8s]: %(message)s',
-#             'datefmt': '%Y-%m-%d %H:%M:%S'
-#         }
-#     },
-#     'filters': {
-#
-#     },
-#     'handlers': {
-#         'file': {
-#             'level': 'INFO',
-#             'class': 'logging.FileHandler',
-#             # 'filename': str(Path(os.path.abspath(__file__)).parent.parent) + '/train.log'
-#             'filename':  + '/train.log'
-#
-#         },
-#         'console': {
-#             'level': 'INFO',
-#             'formatter': 'default',
-#             'class': 'logging.StreamHandler'
-#         }
-#     },
-#     'root': {
-#         'level': 'INFO',
-#         'handlers': ['console', 'file']
-#     }
-# }
-#
-# logging.config.dictConfig(LOGGING)
